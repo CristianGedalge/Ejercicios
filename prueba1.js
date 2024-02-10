8f48c0b395b48534b131eda87677565fce3721d0
CristianGedalge <124637610+CristianGedalge@users.noreply.github.com>
2024-02-10 09:13:31 -0400

    Ejercicio 1

---
+++ prueba1.js
@@ -0,0 +1,23 @@
+"use strict"
+
+
+
+function convertToRoman(num) {
+    let v=[['I','II','III','IV','V','VI','VII','VIII','IX'],['X',"XX","XXX","XL","L","LX","LXX","LXXX","XC"],['C','CC','CCC','CD','D','DC','DCC','DCCC','CM'],['M','MM','MMM']]
+  let s="";
+  while(num!=0)
+  {
+    let n=parseInt(Math.log10(num))+1
+    let dividendo=Math.pow(10,n-1);
+    let cociente=parseInt(num/dividendo);
+    s+=v[n-1][cociente-1]
+    num=num%dividendo;
+
+
+  }
+
+   return s;
+  }
+
+  console.log(convertToRoman(3999));
+
